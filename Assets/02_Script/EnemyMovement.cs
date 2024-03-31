139326bc4114287bd14eaf45708551976d28f07b
plmknbuhv <plmknbuhv@gmail.coom>
2024-03-31 22:31:29 +0900

    적 체력바 추가와 체력 감소 및 적 제거

--- Assets/02_Script/EnemyMovement.cs
+++ Assets/02_Script/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.TerrainUtils;
@@ -9,13 +10,17 @@ using UnityEngine.Tilemaps;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private Tilemap _tilemap;
+    [SerializeField] private HpBar _HpBar;
     [SerializeField] private TileBase _tile;
     [SerializeField] private Vector3Int _beforeDirection;
     [SerializeField] private float _moveSpeed;
+    private Transform _transform;
 
     private void Awake()
     {
         _tilemap = GameObject.Find("Route").GetComponent<Tilemap>();
+        _HpBar = transform.Find("HpBar").GetComponent<HpBar>();
+        _transform = transform.Find("Visual");
     }
 
     private void Start()
@@ -23,50 +28,77 @@ public class EnemyMovement : MonoBehaviour
         StartCoroutine(FindRoute());
     }
 
+    private void Update()
+    {
+        if (_HpBar.curHp <= 0)
+        {
+            Destroy(gameObject);
+            DOTween.Kill(transform);   
+        }
+    }
+
     private IEnumerator FindRoute() 
     {
         while (true) // 무한 반복문
         {
-            yield return null; // 아무 것도 리턴하지 않으면 빠르게 반복해 터지니까 한틱씩 리턴
-
-            if (Input.GetKeyDown(KeyCode.F12)) // 만약 f12를 누르면 
-                break; // 코드 탈출
-
-            foreach (var dir in Directions.DIRECTIONS) // 방향 배열을 반복
+            if (gameObject != null)
             {
-                Vector3Int tilepos = _tilemap.WorldToCell(transform.position); // Enemy의 위치를 Vector3Int로 바꿔 저장
-                tilepos += dir; // 상, 하, 좌, 우 4방향의 타일을 체크
 
-                var tile = _tilemap.GetTile(tilepos); // 체크한 타일을 저장
+                yield return null; // 아무 것도 리턴하지 않으면 빠르게 반복해 터지니까 한틱씩 리턴
 
-                if (tile != null) // 타일이 있다면
+                if (Input.GetKeyDown(KeyCode.F12)) // 만약 f12를 누르면 
+                    break; // 코드 탈출
+
+                foreach (var dir in Directions.DIRECTIONS) // 방향 배열을 반복
                 {
-                    if (dir == -_beforeDirection) continue; // 내가 왔던 길의 타일이라면 스킵
+                    Vector3Int tilepos = _tilemap.WorldToCell(transform.position); // Enemy의 위치를 Vector3Int로 바꿔 저장
+                    tilepos += dir; // 상, 하, 좌, 우 4방향의 타일을 체크
 
-                    /*각 이동 방향별로 Enemy의 방향 전환*/
-                    if (dir == Directions.DIRECTIONS[0]) 
-                    {
-                        transform.rotation = Quaternion.Euler(0, 0, 0);
-                    }
-                    else if (dir == Directions.DIRECTIONS[1])
-                    {
-                        transform.rotation = Quaternion.Euler(0, 0, 180);
-                    }
-                    else if (dir == Directions.DIRECTIONS[2])
-                    {
-                        transform.rotation = Quaternion.Euler(0, 0, 270);
-                    }
-                    else if (dir == Directions.DIRECTIONS[3])
-                    {
-                        transform.rotation = Quaternion.Euler(0, 0, 90);
-                    }
+                    var tile = _tilemap.GetTile(tilepos); // 체크한 타일을 저장
 
-                    transform.DOMove(tilepos, 1 / _moveSpeed).SetEase(Ease.Linear);
+                    if (tile != null) // 타일이 있다면
+                    {
+                        if (dir == -_beforeDirection) continue; // 내가 왔던 길의 타일이라면 스킵
 
-                    _beforeDirection = dir;
-                    yield return new WaitForSeconds(1 / _moveSpeed);
+                        /*각 이동 방향별로 Enemy의 방향 전환*/
+                        if (dir == Directions.DIRECTIONS[0])
+                        {
+                            _transform.rotation = Quaternion.Euler(0, 0, 0);
+                        }
+                        else if (dir == Directions.DIRECTIONS[1])
+                        {
+                            _transform.rotation = Quaternion.Euler(0, 0, 180);
+                        }
+                        else if (dir == Directions.DIRECTIONS[2])
+                        {
+                            _transform.rotation = Quaternion.Euler(0, 0, 270);
+                        }
+                        else if (dir == Directions.DIRECTIONS[3])
+                        {
+                            _transform.rotation = Quaternion.Euler(0, 0, 90);
+                        }
+
+                        transform.DOMove(tilepos, 1 / _moveSpeed).SetEase(Ease.Linear);
+
+                        _beforeDirection = dir;
+                        yield return new WaitForSeconds(1 / _moveSpeed);
+                    }
+                    else
+                    {
+
+                        Destroy(gameObject);
+                    }
                 }
             }
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            _HpBar.Hit();
         }
     }
 }
--- Assets/02_Script/HpBar.cs
+++ Assets/02_Script/HpBar.cs
@@ -6,5 +6,19 @@ using UnityEngine.UI;
 public class HpBar : MonoBehaviour
 {
     private float maxHp = 100;
-    private float curHp = 100;
+    public float curHp = 100;
+    Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform.Find("Pivot").transform;
+
+        _transform.localScale = Vector3.one;
+    }
+
+    public void Hit()
+    {
+        curHp -= 50;
+        _transform.localScale = new Vector3((float)curHp / (float)maxHp, 1, 1);
+    }
 }
