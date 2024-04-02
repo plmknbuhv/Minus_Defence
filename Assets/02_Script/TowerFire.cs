cd01795e3c0f48dd5604dcc4a4cd50125f801100
plmknbuhv <plmknbuhv@gmail.coom>
2024-04-02 21:38:54 +0900

    타워 길 위에 설치 방지

--- Assets/02_Script/TowerFire.cs
+++ Assets/02_Script/TowerFire.cs
@@ -1,14 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class TowerFire : MonoBehaviour
 {
     private bool isDelay;
     [SerializeField] private GameObject _bullet;
-    [HideInInspector] public Collider2D _collision;
     [HideInInspector] public GameObject _obj;
+    [SerializeField] private Collider2D _distanceCol;
     public bool _canFire = false;
     Vector3 point;
 
@@ -32,9 +30,14 @@ public class TowerFire : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        _distanceCol.enabled = false;
+    }
+
     private void Update()
     {
-        if (!_canFire) 
+        if (!_canFire)
         {
             point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
                 Input.mousePosition.y, -Camera.main.transform.position.z));
@@ -42,13 +45,22 @@ public class TowerFire : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
+                Collider2D col = Physics2D.OverlapCircle(point, 0.4f);
+                bool isContact = col == null;
+                //bool isContact = Physics2D.OverlapCircle(point, 0.6f);
+                Debug.Log(isContact + col?.gameObject.name);
+                if (!isContact)
+                {
+                    return;
+                }
+                _distanceCol.enabled = true;
                 _canFire = true;
             }
         }
     }
 
     private IEnumerator Fire_Delay()
-    {   
+    {
         yield return new WaitForSeconds(1f);
         isDelay = false;
     }
